775bebe5f1699d77d712eb7c9b5301b6a70db683
saulgame2010 <saulgame2010@gmail.com>
2023-06-12 11:32:20 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '05-pipes-app';
+  public title = 'Saúl García Medina';
 }
